9e23c177fcceca140f751a754d5976d86ee05dec
Matt Dambra <mpdambra@gmail.com>
2020-02-08 13:38:22 -0500

    adding stuffs

--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
 var path = require("path");
 var inquire = require("inquire");
 var express = require("express");
-var fs = require ("fs");
+var fs = require("fs");
 const PORT = 8888;
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  app.get("/tables", function(req, res) {
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../home.html"));
+});
+app.get("/tables", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "tables.html"));
-  });
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-  var reservation = [];
-  var waitingList = [];
-  app.post("/reserve.html", function(req, res) {
-    let responseReservation = req.body;
-    let newReservation = new reservation(responseReservation.id, responseReservation.eventNames, 
-        responseReservation.email, responseReservation.phoneumber);
-    });
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+var reservation = [];
+var waitingList = [];
+app.post("/reserve.html", function (req, res) {
+  let responseReservation = req.body;
+  let newReservation = new reservation(responseReservation.id, responseReservation.eventNames,
+    responseReservation.email, responseReservation.phoneumber);
+});
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 })
