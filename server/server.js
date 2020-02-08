597e17a077b685ce4fde6af7c8c1a81c0031ef74
Shane Keney <shane@spikeball.com>
2020-02-08 13:33:07 -0500

    - push change

--- server/server.js
+++ server/server.js
@@ -2,29 +2,33 @@ var path = require("path");
 var inquire = require("inquire");
 var express = require("express");
 var fs = require ("fs");
+
+var reservation = [];
+var waitingList = [];
+
 const PORT = 8888;
 var app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "templates/home.html"));
+});
 
+app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-  });
+});
 
-  app.get("/reserve", function(req, res) {
+app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-  var reservation = [];
-  var waitingList = [];
-  app.post("/reserve.html", function(req, res) {
+});
+
+app.post("/reserve.html", function(req, res) {
     let responseReservation = req.body;
     let newReservation = new reservation(responseReservation.id, responseReservation.eventNames, 
         responseReservation.email, responseReservation.phoneumber);
-    });
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
